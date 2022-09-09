eadac7899db9169610d3eeb8ff6e44c6c934b291
AlexKalimullin <94666746+AlexKalimullin@users.noreply.github.com>
2022-09-09 11:24:11 +0700

    Add files via upload

---
+++ src/homework/task1.java
@@ -0,0 +1,92 @@
+/*
+Task1
+            Убрать дубликаты, отсортировать по идентификатору, сгруппировать по имени
+
+            Что должно получиться
+                Key:Amelia
+                Value:4
+                Key: Emily
+                Value:1
+                Key: Harry
+                Value:3
+                Key: Jack
+                Value:1
+      */
+
+package homework;
+
+import java.util.*;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+public class task1 {
+
+    static class Person {
+        final int id;
+
+        final String name;
+
+        Person(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Person person)) return false;
+            return getId() == person.getId() && getName().equals(person.getName());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getId(), getName());
+        }
+    }
+
+    private static Person[] RAW_DATA = new Person[]{
+            new Person(0, "Harry"),
+            new Person(0, "Harry"), // дубликат
+            new Person(1, "Harry"), // тёзка
+            new Person(2, "Harry"),
+            new Person(3, "Emily"),
+            new Person(4, "Jack"),
+            new Person(4, "Jack"),
+            new Person(5, "Amelia"),
+            new Person(5, "Amelia"),
+            new Person(6, "Amelia"),
+            new Person(7, "Amelia"),
+            new Person(8, "Amelia"),
+    };
+
+    public static void main(String[] args) {
+        System.out.println("Raw data:");
+        System.out.println();
+
+        for (Person person : RAW_DATA) {
+            System.out.println(person.id + " - " + person.name);
+        }
+
+        System.out.println();
+        System.out.println("**************************************************");
+        System.out.println();
+        System.out.println("Duplicate filtered, grouped by name, sorted by name and id:");
+        System.out.println();
+
+        Map<String, Long> persons = Arrays.stream(RAW_DATA)
+                .distinct()
+                .collect(groupingBy(Person::getName, counting()));
+
+        for (Map.Entry<String, Long> person : persons.entrySet()) {
+            System.out.println("Key:"+ person.getKey() + "\nValue:" + person.getValue());
+        }
+    }
+}
---
+++ src/homework/task3.java
@@ -0,0 +1,47 @@
+/*
+        Task3
+            Реализовать функцию нечеткого поиска
+                    fuzzySearch("car", "ca6$$#_rtwheel"); // true
+                    fuzzySearch("cwhl", "cartwheel"); // true
+                    fuzzySearch("cwhee", "cartwheel"); // true
+                    fuzzySearch("cartwheel", "cartwheel"); // true
+                    fuzzySearch("cwheeel", "cartwheel"); // false
+                    fuzzySearch("lw", "cartwheel"); // false
+         */
+
+package homework;
+
+import java.util.Objects;
+
+public class task3 {
+    public static void fuzzySearch(String s1, String s2){
+        int count = 0;
+        int wordIndex = 0;
+        int phraseIndex = 0;
+
+        String[] wordArray = s1.split("");
+        String[] phraseArray = s2.split("");
+
+        if (s1.length()==0 || s2.length()==0 || s1.length()>s2.length()){
+            System.out.println(false);
+        } else {
+            while (wordIndex < wordArray.length && phraseIndex < phraseArray.length) {
+                if (Objects.equals(wordArray[wordIndex], phraseArray[phraseIndex])) {
+                    count++;
+                    wordIndex++;
+                }
+                phraseIndex++;
+            }
+            System.out.println(count == wordArray.length);
+        }
+    }
+
+    public static void main(String[] args) {
+        fuzzySearch("car", "ca6$$#_rtwheel"); // true
+        fuzzySearch("cwhl", "cartwheel"); // true
+        fuzzySearch("cwhee", "cartwheel"); // true
+        fuzzySearch("cartwheel", "cartwheel"); // true
+        fuzzySearch("cwheeel", "cartwheel"); // false
+        fuzzySearch("lw", "cartwheel"); // false
+    }
+}
